83a679450d65a6e564bc20bff82bd94a8c24a272
Forest <playa200612@gmail.com>
2024-04-24 09:26:28 -0500

    Versión 2

---
+++ src/main/java/org/diagramExercise/Grade.java
@@ -0,0 +1,19 @@
+package org.diagramExercise;
+
+public class Grade {
+    private int grade;
+    private String description;
+
+    public Grade(int grade, String description) {
+        this.grade = grade;
+        this.description = description;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
